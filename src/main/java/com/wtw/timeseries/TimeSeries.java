117681e9992a910741907ae38cafe7b7453ed482
Mackenzie Goodwin <mackenziejamesgoodwin@gmail.com>
2017-06-16 20:13:58 -0400

    Improved TimeSeriesPoint to support toString

--- src/main/java/com/wtw/timeseries/TimeSeries.java
+++ src/main/java/com/wtw/timeseries/TimeSeries.java
@@ -1,4 +1,9 @@
 package com.wtw.timeseries;
 
+import java.util.ArrayList;
+
 public class TimeSeries {
+
+    private ArrayList<TimeSeriesPoint> points = new ArrayList<TimeSeriesPoint>();
+
 }
--- src/main/java/com/wtw/timeseries/TimeSeriesPoint.java
+++ src/main/java/com/wtw/timeseries/TimeSeriesPoint.java
@@ -1,9 +1,14 @@
 package com.wtw.timeseries;
 
+import com.google.common.base.Preconditions;
+import lombok.Getter;
+
 public class TimeSeriesPoint {
 
+    @Getter
     private float[] dimensions;
 
+    @Getter
     private long time;
 
     public TimeSeriesPoint(float[] dimensions, long time) {
@@ -21,19 +26,38 @@ public class TimeSeriesPoint {
         this.time = System.nanoTime();
     }
 
-    public int length() {
+    public int dimensionSize() {
+        Preconditions.checkNotNull(this.dimensions);
+
         return this.dimensions.length;
     }
 
-    public long getTime() {
-        return time;
-    }
-
-    public float[] getDimensions() {
-        return dimensions;
-    }
-
     public float getDimension(int index) {
+        Preconditions.checkPositionIndex(index, this.dimensionSize());
+
         return dimensions[index];
+    }
+
+    public float getDistance(TimeSeriesPoint timeSeriesPoint) {
+        return getDistance(timeSeriesPoint.getDimensions());
+    }
+
+    public float getDistance(float[] points) {
+        Preconditions.checkArgument(this.dimensionSize() == points.length, "Can't compare points with different number of dimensions");
+        Preconditions.checkNotNull(this.dimensions);
+        float sum = 0.0f;
+        for (int i = 0; i < points.length; ++i) {
+            sum += Math.pow(this.dimensions[i] - points[i], 2);
+        }
+        return (float) Math.sqrt(sum);
+    }
+
+    @Override
+    public String toString() {
+        String formatted = "(";
+        for (int i = 0; i < this.dimensionSize(); ++i) {
+            formatted.concat(dimensions[i] + ",");
+        }
+        return formatted + this.time + ")";
     }
 }
